4b7ae3e26055a4a00ad1bfc4d513c364ad730e5e
Ricky Manriquez <dechimp@gmail.com>
2015-07-15 21:03:10 -0700

    enhance passwords with bcrypt

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "Example User", password: "letmein", password_confirmation: "letmein")
+  end
+
+  test "valid password should validate" do
+    @user.password = @user.password_confirmation = "foobarbaz"
+    assert @user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
 end
